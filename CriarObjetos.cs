5a52c84d54316296b3b4be97d3a283dcecdcdc83
Roberto C Soares <65926261+CristianoDsoares@users.noreply.github.com>
2020-09-23 17:06:25 -0300

    Add files via upload

---
+++ CriarObjetos.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CriarObjetos : MonoBehaviour
+{
+    public Vector3 posicao, rotacao;
+    public GameObject prefab;
+    /*
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    */
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+                //criando objetos posionando e girando
+                /*
+            GameObject objeto = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            objeto.transform.position = posicao; 
+            objeto.transform.rotation =  Quaternion.Euler(rotacao);
+            */
+
+            // Criando objetos com prefabs
+            Instantiate(prefab, posicao, Quaternion.Euler(rotacao) );
+
+
+
+        }
+        
+    }
+}
---
+++ ativandoObjetos.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ativandoObjetos : MonoBehaviour
+{
+    public GameObject objeto;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+       if (GetComponent<MeshRenderer>().enabled == false)
+        {
+            Debug.Log("A malha do Objeto Esta desativada");
+        }else
+
+        {
+
+             Debug.Log("A malha do Objeto Esta ativada");
+
+        }
+        
+        
+    }
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Desabilitar Objeto baseado em outro
+        if (objeto.activeInHierarchy == false)
+        {
+             Debug.Log("O cubo esta inativo");
+        }
+           else
+            {
+                Debug.Log("O Cubo esta Ativo"); 
+            
+            
+            }  
+  
+  
+    }  
+    
+
+
+
+}
